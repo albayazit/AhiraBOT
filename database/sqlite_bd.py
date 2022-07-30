a4dd54fb8876b124f86f21ed31e1ed7e0af21f44
Bayazit <bakhasanov@gmail.com>
2022-07-30 13:41:07 +0300

    Create db table for favorite cities

--- database/sqlite_bd.py
+++ database/sqlite_bd.py
@@ -2,10 +2,9 @@ import sqlite3 as sq
 
 def sql_start():
 	global base, cur
-	base = sq.connect('ahira_users.db')
+	base = sq.connect('database.db')
 	cur = base.cursor()
 	if base:
 		print('База данных подключена!')
-	base.execute('CREATE TABLE IF NOT EXISTS user(user_id TEXT PRIMARY KEY, city TEXT)')
+	base.execute('CREATE TABLE IF NOT EXISTS cities(city_id TEXT, user_id TEXT, city TEXT)')
 	base.commit()
-
