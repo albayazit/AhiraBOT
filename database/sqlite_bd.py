110e7b67fb1b08f07fc9036179649bd54bb865f6
Bayazit <bakhasanov@gmail.com>
2022-08-10 19:04:58 +0300

    Created table for Kazakhstan cities

--- database/sqlite_bd.py
+++ database/sqlite_bd.py
@@ -9,4 +9,6 @@ def sql_start():
 	base.execute('CREATE TABLE IF NOT EXISTS favorite_other(user_id TEXT, address TEXT,school TEXT)')
 	base.commit()
 	base.execute('CREATE TABLE IF NOT EXISTS favorite_tatarstan(user_id TEXT, address TEXT)')
-	base.commit()	
+	base.commit()
+	base.execute('CREATE TABLE IF NOT EXISTS favorite_kazakhstan(user_id TEXT, address TEXT)')
+	base.commit()
