63b6e2a0733dd5e349354e4985bc1603e17c0cde
Bayazit <bakhasanov@gmail.com>
2022-08-10 12:03:59 +0300

    Created new table for tatarstan cities

--- database/sqlite_bd.py
+++ database/sqlite_bd.py
@@ -8,3 +8,5 @@ def sql_start():
 		print('База данных подключена!')
 	base.execute('CREATE TABLE IF NOT EXISTS favorite_other(user_id TEXT, address TEXT,school TEXT)')
 	base.commit()
+	base.execute('CREATE TABLE IF NOT EXISTS favorite_tatarstan(user_id TEXT, address TEXT)')
+	base.commit()	
