14bd13d8c8c76cc3ffb9440ea91f101c5d01d610
Bayazit <bakhasanov@gmail.com>
2022-08-14 23:03:37 +0300

    Created table for tracker

--- database/sqlite_bd.py
+++ database/sqlite_bd.py
@@ -14,3 +14,5 @@ def sql_start():
 	base.commit()
 	base.execute('CREATE TABLE IF NOT EXISTS favorite_dagestan(user_id TEXT, address TEXT)')
 	base.commit()
+	base.execute('CREATE TABLE IF NOT EXISTS tracker(user_id TEXT, fajr TEXT, fajr_need TEXT, zuhr TEXT, zuhr_need TEXT, asr TEXT, asr_need TEXT, magrib TEXT, magrib_need TEXT, isha TEXT, isha_need TEXT, vitr TEXT, vitr_need TEXT, first_date TEXT, second_date TEXT)')
+	base.commit()
