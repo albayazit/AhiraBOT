f223b4cb807d2a1cf6a424f0c9febd85fbf2f271
Bayazit <bakhasanov@gmail.com>
2022-08-26 18:55:06 +0300

    Added id stroke in hadis table

--- database/sqlite_bd.py
+++ database/sqlite_bd.py
@@ -18,5 +18,5 @@ def sql_start():
 	base.commit()
 	base.execute('CREATE TABLE IF NOT EXISTS zikr(user_id TEXT, zikr_1_today TEXT, zikr_1_all, zikr_2_today TEXT, zikr_2_all, zikr_3_today TEXT, zikr_3_all, zikr_4_today TEXT, zikr_4_all, zikr_5_today TEXT, zikr_5_all, zikr_6_today TEXT, zikr_6_all, zikr_7_today TEXT, zikr_7_all, zikr_8_today TEXT, zikr_8_all, zikr_9_today TEXT, zikr_9_all, zikr_10_today TEXT, zikr_10_all, zikr_11_today TEXT, zikr_11_all, zikr_12_today TEXT, zikr_12_all, zikr_13_today TEXT, zikr_13_all, zikr_14_today TEXT, zikr_14_all, zikr_15_today TEXT, zikr_15_all, zikr_16_today TEXT, zikr_16_all)')
 	base.commit()
-	base.execute('CREATE TABLE IF NOT EXISTS hadis(user_id TEXT, hadis_id TEXT)')
+	base.execute('CREATE TABLE IF NOT EXISTS hadis(user_id TEXT, hadis_id TEXT, id TEXT)')
 	base.commit()
