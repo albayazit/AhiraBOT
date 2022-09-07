41c772fd50dcc7464243b72b0ae393beb4038354
Bayazit <bakhasanov@gmail.com>
2022-09-08 01:02:09 +0300

    Fixed halal food message

--- parcer/parcer_food.py
+++ parcer/parcer_food.py
@@ -7,11 +7,10 @@ async def get_message(page):
 		for i in range(key - 5, key):
 			id = sqlite_bd.cur.execute('SELECT id FROM halal_food WHERE id == ?', (i+1, )).fetchone()[0]
 			name = sqlite_bd.cur.execute('SELECT name FROM halal_food WHERE id == ?', (i+1, )).fetchone()[0]
-			company = sqlite_bd.cur.execute('SELECT company FROM halal_food WHERE id == ?', (i+1, )).fetchone()[0]
 			period = sqlite_bd.cur.execute('SELECT period FROM halal_food WHERE id == ?', (i+1, )).fetchone()[0]
 			address = sqlite_bd.cur.execute('SELECT address FROM halal_food WHERE id == ?', (i+1, )).fetchone()[0]
 
-			message += f'{id}. {name}\n{company}\n{period}\n{address}\n\n'
+			message += f'{id}. {name}\nСрок действия: {period}\nАдрес: {address}\n\n'
 	except:
 		pass
 	return message
