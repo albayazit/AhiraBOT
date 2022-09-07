40f39caa51702a77ec0acd1e568121a6e2916818
Bayazit <bakhasanov@gmail.com>
2022-09-07 22:49:08 +0300

    Added script for scrapping codes to db from excel

---
+++ codes_script.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from database import sqlite_bd
+	
+data = pd.read_excel('data/halal/halal_list.xlsx')
+sqlite_bd.sql_start()
+sqlite_bd.cur.execute('')
+
+for i in range(100, 1000):
+	pd.set_option('display.max_colwidth', 500)
+	info = data[data['Код'] == 'E'+str(i)]
+	info['Дозволенность'] = info['Дозволенность'].str.replace(r'\n', '')
+	info['Описание'] = info['Описание'].str.replace(r'\n', '')
+	code = info["Код"].to_string(index = False)
+	description = info["Описание"].to_string(index = False)
+	permissiveness = info["Дозволенность"].to_string(index = False)
+	if code[0] == 'E':
+		sqlite_bd.cur.execute('INSERT INTO halal_codes VALUES(?, ?, ?)', (code, description, permissiveness))
+		sqlite_bd.base.commit()
+	else:
+		pass
+
+print('База успешно загружена!')
